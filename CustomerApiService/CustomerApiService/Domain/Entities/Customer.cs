dcbf58902c54c4f61c0f98c45e2ef3884828922b
Alireza <s.alireza.mohammadi@gmail.com>
2020-11-09 22:41:33 +0000

    Add Customer domain project

---
+++ CustomerApiService/CustomerApiService/Domain/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Customer: IEntity
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Birthday { get; set; }
+        public int? Age { get; set; }
+    }
+}
