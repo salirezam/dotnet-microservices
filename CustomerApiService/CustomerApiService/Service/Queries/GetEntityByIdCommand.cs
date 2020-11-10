5b4e09dfdf113ade2ef6a7043fe7a5ef37a22f56
Alireza <s.alireza.mohammadi@gmail.com>
2020-11-10 21:30:45 +0000

    Add customer queries

---
+++ CustomerApiService/CustomerApiService/Service/Queries/GetEntityByIdCommand.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using MediatR;
+using System;
+
+namespace Service.Queries
+{
+    public class GetEntityByIdCommand<T>: IRequest<T>
+        where T: class, IEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ CustomerApiService/CustomerApiService/Service/Queries/GetEntityByIdCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Data.Repository;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Service.Queries
+{
+    public class GetEntityByIdCommandHandler<T> : IRequestHandler<GetEntityByIdCommand<T>, T>
+        where T: class, IEntity
+    {
+        private readonly IRepository<T> repository;
+
+        public GetEntityByIdCommandHandler(IRepository<T> repository)
+        {
+            this.repository = repository;
+        }
+        public Task<T> Handle(GetEntityByIdCommand<T> request, CancellationToken cancellationToken)
+        {
+            return repository.Get(request.Id);
+        }
+    }
+}
