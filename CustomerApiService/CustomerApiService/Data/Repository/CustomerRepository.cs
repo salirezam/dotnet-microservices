0c9ff503f2e0dbce6846625f6ae920dbc57c4c67
Alireza <s.alireza.mohammadi@gmail.com>
2020-11-10 17:17:33 +0000

    Add Customer Repository

--- CustomerApiService/CustomerApiService/Data/Repository/CustomerRepository.cs
+++ CustomerApiService/CustomerApiService/Data/Repository/CustomerRepository.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Data.Database;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Data.Repository
 {
-    class CustomerRepository
+    public class CustomerRepository: Repository<Customer, CustomerContext>
     {
+        public CustomerRepository(CustomerContext context): base(context)
+        {
+
+        }
     }
 }
