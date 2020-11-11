3ed557d96997bcdc2af4d2d23e0025c7edbdb9b3
Alireza <s.alireza.mohammadi@gmail.com>
2020-11-11 10:42:28 +0000

    Add create and update dtos

---
+++ CustomerApiService/CustomerApiService/CustomerApiService/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApiService.Dtos
+{
+    public class CreateCustomerDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime? Birthday { get; set; }
+        public int? Age { get; set; }
+    }
+}
---
+++ CustomerApiService/CustomerApiService/CustomerApiService/Dtos/UpdateCustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApiService.Dtos
+{
+    public class UpdateCustomerDto
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime? Birthday { get; set; }
+        public int? Age { get; set; }
+    }
+}
