a7ce2be80b6c52c6fa5918f05c0e05fcb3ccaf1f
Alireza <s.alireza.mohammadi@gmail.com>
2020-11-11 10:42:05 +0000

    Add AutoMapper profile

---
+++ CustomerApiService/CustomerApiService/CustomerApiService/Infrastructures/AutoMapper/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CustomerApiService.Dtos;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApiService.Infrastructures.AutoMapper
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateCustomerDto, Customer>().ForMember(x => x.Id, opt => opt.Ignore());
+            CreateMap<UpdateCustomerDto, Customer>();
+        }
+    }
+}
